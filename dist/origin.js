524b943b4f07c3a31634a0c4d53f2c6b15283c4a
CisThard <he3717@gmail.com>
2024-11-14 02:53:17 +0700

    Changed : Endpoint to retrieve foundation and bounty details when the foundationID parameter is provided

--- dist/origin.js
+++ dist/origin.js
@@ -8,6 +8,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -19,7 +30,20 @@ const port = 4000;
 const asyncHandler = (fn) => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
 };
-// 기존 /get-object/:objectId 엔드포인트 (패키지 ID로만 조회)
+// Store the connected wallet address (this can be changed to store in DB or session)
+let connectedWalletAddress = null;
+// Endpoint to set the connected wallet address
+app.post('/set-connected-wallet-address', asyncHandler((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { walletAddress } = req.body;
+    if (!walletAddress) {
+        return res.status(400).send("Wallet address is required");
+    }
+    // Store the wallet address
+    connectedWalletAddress = walletAddress;
+    console.log(`Connected wallet address: ${connectedWalletAddress}`);
+    res.status(200).send({ message: "Wallet address set successfully" });
+})));
+// Existing /get-object/:objectId endpoint
 app.get('/get-object/:objectId', asyncHandler((req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { objectId } = req.params;
     if (!objectId) {
@@ -29,17 +53,15 @@ app.get('/get-object/:objectId', asyncHandler((req, res) => __awaiter(void 0, vo
     if (!result) {
         return res.status(404).send("Object not found");
     }
-    // 원래 결과만 반환
     res.json(result);
 })));
-// 기존 /get-foundation-details/:objectId 엔드포인트
+// Existing /get-foundation-details/:objectId endpoint
 app.get('/get-foundation-details/:objectId', asyncHandler((req, res) => __awaiter(void 0, void 0, void 0, function* () {
     var _a, _b;
     const { objectId } = req.params;
     if (!objectId) {
         return res.status(400).send("Object ID is required");
     }
-    // 주어진 objectId로 조회하여 foundation_ids 가져오기
     const result = yield (0, object_1.getObject)(objectId);
     if (!result) {
         return res.status(404).send("Object not found");
@@ -48,26 +70,22 @@ app.get('/get-foundation-details/:objectId', asyncHandler((req, res) => __awaite
     if (!foundationIds || !Array.isArray(foundationIds) || foundationIds.length === 0) {
         return res.status(404).send("No foundation IDs found in the object");
     }
-    // foundation_ids로 추가 조회 수행
     const foundationDetailsPromises = foundationIds.map((foundationId) => __awaiter(void 0, void 0, void 0, function* () {
         const foundationData = yield (0, object_1.getObject)(foundationId);
         return foundationData;
     }));
-    // 모든 foundation 상세 데이터를 비동기로 조회
     const foundationDetails = yield Promise.all(foundationDetailsPromises);
-    // 원래 결과와 foundationDetails 추가하여 반환
     res.json({
         foundationDetails
     });
 })));
-// 새로운 /get-foundation-details-with-bounty/:objectId 엔드포인트
-app.get('/get-foundation-details-with-bounty/:objectId', asyncHandler((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+// Retrieve detailed bounty information
+app.get('/get-bounty-details/:objectId', asyncHandler((req, res) => __awaiter(void 0, void 0, void 0, function* () {
     var _c, _d;
     const { objectId } = req.params;
     if (!objectId) {
         return res.status(400).send("Object ID is required");
     }
-    // 주어진 objectId로 조회하여 foundation_ids 가져오기
     const result = yield (0, object_1.getObject)(objectId);
     if (!result) {
         return res.status(404).send("Object not found");
@@ -76,33 +94,143 @@ app.get('/get-foundation-details-with-bounty/:objectId', asyncHandler((req, res)
     if (!foundationIds || !Array.isArray(foundationIds) || foundationIds.length === 0) {
         return res.status(404).send("No foundation IDs found in the object");
     }
-    // foundation_ids로 추가 조회 수행
     const foundationDetailsPromises = foundationIds.map((foundationId) => __awaiter(void 0, void 0, void 0, function* () {
         const foundationData = yield (0, object_1.getObject)(foundationId);
         return foundationData;
     }));
-    // 모든 foundation 상세 데이터를 비동기로 조회
     const foundationDetails = yield Promise.all(foundationDetailsPromises);
-    // 각 foundation의 bounty_table_keys를 사용해 추가 조회
     const bountyDetailsPromises = foundationDetails.map((foundationDetail) => __awaiter(void 0, void 0, void 0, function* () {
         var _e, _f;
         const bountyTableKeys = (_f = (_e = foundationDetail.content) === null || _e === void 0 ? void 0 : _e.fields) === null || _f === void 0 ? void 0 : _f.bounty_table_keys;
         if (!bountyTableKeys || !Array.isArray(bountyTableKeys) || bountyTableKeys.length === 0) {
             return null;
         }
-        // bounty_table_keys로 추가 조회 수행
         const bountyDetails = yield Promise.all(bountyTableKeys.map((bountyKey) => __awaiter(void 0, void 0, void 0, function* () {
             const bountyData = yield (0, object_1.getObject)(bountyKey);
             return bountyData;
         })));
         return bountyDetails;
     }));
-    // 모든 bounty 데이터 비동기로 조회
     const bountyDetails = yield Promise.all(bountyDetailsPromises);
-    // 원래 결과, foundationDetails, 그리고 bountyDetails를 추가하여 반환
     res.json({
         bountyDetails
     });
+})));
+const filterFields = (obj, fieldsToRemove) => {
+    if (typeof obj !== 'object' || obj === null)
+        return obj;
+    const filteredObj = Array.isArray(obj) ? [] : {}; // 객체 타입을 명시적으로 지정
+    for (const key in obj) {
+        if (fieldsToRemove.includes(key))
+            continue;
+        filteredObj[key] = filterFields(obj[key], fieldsToRemove);
+    }
+    return filteredObj;
+};
+// /get-foundation-bounty-details/:foundationId 엔드포인트
+app.get('/get-foundation-bounty-details/:foundationId', asyncHandler((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { foundationId } = req.params;
+    if (!foundationId) {
+        return res.status(400).send("Foundation ID is required");
+    }
+    // Foundation 객체에서 데이터 가져오기
+    const foundationResult = yield (0, object_1.getObject)(foundationId);
+    if (!foundationResult) {
+        return res.status(404).send("Foundation not found");
+    }
+    // foundationDetails에서 bounty_table과 bounty_table_keys 제거
+    const _g = foundationResult.content.fields, { bounty_table, bounty_table_keys } = _g, otherFields = __rest(_g, ["bounty_table", "bounty_table_keys"]);
+    const filteredFoundationDetails = Object.assign(Object.assign({}, foundationResult), { content: Object.assign(Object.assign({}, foundationResult.content), { fields: otherFields }) });
+    // bounty_table_keys의 각 키를 사용하여 bountyDetails 가져오기
+    const bountyDetailsPromises = (bounty_table_keys || []).map((bountyKey) => __awaiter(void 0, void 0, void 0, function* () {
+        return yield (0, object_1.getObject)(bountyKey);
+    }));
+    const bountyDetails = yield Promise.all(bountyDetailsPromises);
+    // bountyDetails를 foundationDetails 내부에 포함
+    const finalResult = {
+        foundationDetails: Object.assign(Object.assign({}, filteredFoundationDetails), { content: Object.assign(Object.assign({}, filteredFoundationDetails.content), { fields: Object.assign(Object.assign({}, filteredFoundationDetails.content.fields), { bountyDetails: bountyDetails.filter(Boolean) // null 값을 제외하고 반환
+                 }) }) })
+    };
+    // digest, version, dataType, hasPublicTransfer 필드를 제거
+    const fieldsToRemove = ['digest', 'version', 'dataType', 'hasPublicTransfer'];
+    const filteredResult = filterFields(finalResult, fieldsToRemove);
+    res.json(filteredResult);
+})));
+// // foundationId로 foundation과 bounty 상세 정보를 얻는 새로운 엔드포인트
+// app.get('/get-foundation-bounty-details/:foundationId', asyncHandler(async (req: Request, res: Response) => {
+//     const { foundationId } = req.params;
+//     if (!foundationId) {
+//         return res.status(400).send("Foundation ID is required");
+//     }
+//     // Foundation 객체에서 데이터 가져오기
+//     const foundationResult = await getObject(foundationId);
+//     if (!foundationResult) {
+//         return res.status(404).send("Foundation not found");
+//     }
+//     // foundationDetails에서 bounty_table과 bounty_table_keys를 제거
+//     const { bounty_table, bounty_table_keys, ...otherFields } = foundationResult.content.fields;
+//     const filteredFoundationDetails = {
+//         ...foundationResult,
+//         content: {
+//             ...foundationResult.content,
+//             fields: otherFields
+//         }
+//     };
+//     // bounty_table_keys의 각 키를 사용하여 bountyDetails 가져오기
+//     const bountyDetailsPromises = (bounty_table_keys || []).map(async (bountyKey: string) => {
+//         return await getObject(bountyKey);
+//     });
+//     const bountyDetails = await Promise.all(bountyDetailsPromises);
+//     // bountyDetails를 foundationDetails 내부에 포함
+//     const finalResult = {
+//         foundationDetails: {
+//             ...filteredFoundationDetails,
+//             content: {
+//                 ...filteredFoundationDetails.content,
+//                 fields: {
+//                     ...filteredFoundationDetails.content.fields,
+//                     bountyDetails: bountyDetails.filter(Boolean) // null 값을 제외하고 반환
+//                 }
+//             }
+//         }
+//     };
+//     res.json(finalResult);
+// }));
+// New endpoint to retrieve completed bounty details
+app.get('/get-completed-bounty-details/:objectId', asyncHandler((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _h, _j;
+    const { objectId } = req.params;
+    const result = yield (0, object_1.getObject)(objectId);
+    const completedIds = ((_j = (_h = result.content) === null || _h === void 0 ? void 0 : _h.fields) === null || _j === void 0 ? void 0 : _j.completed) || [];
+    const completedDetails = yield Promise.all(completedIds.map((completedId) => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield (0, object_1.getObject)(completedId);
+        return data;
+    })));
+    res.json({ completedDetails });
+})));
+// New endpoint to retrieve processing bounty details
+app.get('/get-processing-bounty-details/:objectId', asyncHandler((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _k, _l;
+    const { objectId } = req.params;
+    const result = yield (0, object_1.getObject)(objectId);
+    const processingIds = ((_l = (_k = result.content) === null || _k === void 0 ? void 0 : _k.fields) === null || _l === void 0 ? void 0 : _l.processing) || [];
+    const processingDetails = yield Promise.all(processingIds.map((processingId) => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield (0, object_1.getObject)(processingId);
+        return data;
+    })));
+    res.json({ processingDetails });
+})));
+// New endpoint to retrieve unconfirmed proposal bounty details
+app.get('/get-unconfirmed-bounty-details/:objectId', asyncHandler((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _m, _o;
+    const { objectId } = req.params;
+    const result = yield (0, object_1.getObject)(objectId);
+    const unconfirmedProposalIds = ((_o = (_m = result.content) === null || _m === void 0 ? void 0 : _m.fields) === null || _o === void 0 ? void 0 : _o.unconfirmedProposal) || [];
+    const unconfirmedProposalDetails = yield Promise.all(unconfirmedProposalIds.map((proposalId) => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield (0, object_1.getObject)(proposalId);
+        return data;
+    })));
+    res.json({ unconfirmedProposalDetails });
 })));
 app.listen(port, () => {
     console.log(`Server is running on http://0.0.0.0:${port}`);
--- src/origin.ts
+++ src/origin.ts
@@ -9,12 +9,35 @@ const asyncHandler = (fn: (req: Request, res: Response, next: NextFunction) => P
         Promise.resolve(fn(req, res, next)).catch(next);
     };
 
-// Existing /get-object/:objectId endpoint (only retrieves by package ID)
+
+// Store the connected wallet address (this can be changed to store in DB or session)
+let connectedWalletAddress: string | null = null;
+
+// Endpoint to set the connected wallet address
+app.post('/set-connected-wallet-address', asyncHandler(async (req: Request, res: Response) => {
+    const { walletAddress } = req.body;
+    if (!walletAddress) {
+        return res.status(400).send("Wallet address is required");
+    }
+
+    // Store the wallet address
+    connectedWalletAddress = walletAddress;
+
+    console.log(`Connected wallet address: ${connectedWalletAddress}`);
+    res.status(200).send({ message: "Wallet address set successfully" });
+}));
+
+// Existing /get-object/:objectId endpoint
 app.get('/get-object/:objectId', asyncHandler(async (req: Request, res: Response) => {
     const { objectId } = req.params;
 
     if (!objectId) {
         return res.status(400).send("Object ID is required");
+    }
+
+    // Use the connected wallet address here if needed
+    if (connectedWalletAddress) {
+        console.log(`Using connected wallet address: ${connectedWalletAddress}`);
     }
 
     const result = await getObject(objectId);
@@ -34,7 +57,6 @@ app.get('/get-foundation-details/:objectId', asyncHandler(async (req: Request, r
         return res.status(400).send("Object ID is required");
     }
 
-    // Retrieve foundation_ids by looking up the given objectId
     const result = await getObject(objectId);
 
     if (!result) {
@@ -47,16 +69,13 @@ app.get('/get-foundation-details/:objectId', asyncHandler(async (req: Request, r
         return res.status(404).send("No foundation IDs found in the object");
     }
 
-    // Perform additional queries using foundation_ids
     const foundationDetailsPromises = foundationIds.map(async (foundationId: string) => {
         const foundationData = await getObject(foundationId);
         return foundationData;
     });
 
-    // Asynchronously retrieve all foundation details
     const foundationDetails = await Promise.all(foundationDetailsPromises);
 
-    // Return the foundation details
     res.json({
         foundationDetails
     });
@@ -70,7 +89,6 @@ app.get('/get-bounty-details/:objectId', asyncHandler(async (req: Request, res: 
         return res.status(400).send("Object ID is required");
     }
 
-    // Retrieve foundation_ids by looking up the given objectId
     const result = await getObject(objectId);
 
     if (!result) {
@@ -83,16 +101,13 @@ app.get('/get-bounty-details/:objectId', asyncHandler(async (req: Request, res: 
         return res.status(404).send("No foundation IDs found in the object");
     }
 
-    // Perform additional queries using foundation_ids
     const foundationDetailsPromises = foundationIds.map(async (foundationId: string) => {
         const foundationData = await getObject(foundationId);
         return foundationData;
     });
 
-    // Asynchronously retrieve all foundation details
     const foundationDetails = await Promise.all(foundationDetailsPromises);
 
-    // Use each foundation's bounty_table_keys for additional queries
     const bountyDetailsPromises = foundationDetails.map(async (foundationDetail: any) => {
         const bountyTableKeys = foundationDetail.content?.fields?.bounty_table_keys;
 
@@ -100,7 +115,6 @@ app.get('/get-bounty-details/:objectId', asyncHandler(async (req: Request, res: 
             return null;
         }
 
-        // Perform additional queries using bounty_table_keys
         const bountyDetails = await Promise.all(bountyTableKeys.map(async (bountyKey: string) => {
             const bountyData = await getObject(bountyKey);
             return bountyData;
@@ -109,13 +123,176 @@ app.get('/get-bounty-details/:objectId', asyncHandler(async (req: Request, res: 
         return bountyDetails;
     });
 
-    // Asynchronously retrieve all bounty data
     const bountyDetails = await Promise.all(bountyDetailsPromises);
 
-    // Return the bounty details
     res.json({
         bountyDetails
     });
+}));
+
+const filterFields = (obj: any, fieldsToRemove: string[]): any => {
+    if (typeof obj !== 'object' || obj === null) return obj;
+    const filteredObj: Record<string, any> = Array.isArray(obj) ? [] : {}; // 객체 타입을 명시적으로 지정
+    for (const key in obj) {
+        if (fieldsToRemove.includes(key)) continue;
+        filteredObj[key] = filterFields(obj[key], fieldsToRemove);
+    }
+    return filteredObj;
+};
+
+// digest, version, dataType, hasPublicTransfer 있는 버전
+// foundationID 파라미터 전달 시 foundation과 bounty 상세 정보를 가져오는 새로운 엔드포인트
+app.get('/get-foundation-bounty-details/:foundationId', asyncHandler(async (req: Request, res: Response) => {
+    const { foundationId } = req.params;
+
+    if (!foundationId) {
+        return res.status(400).send("Foundation ID is required");
+    }
+
+    // Foundation 객체에서 데이터 가져오기
+    const foundationResult = await getObject(foundationId);
+
+    if (!foundationResult) {
+        return res.status(404).send("Foundation not found");
+    }
+
+    // foundationDetails에서 bounty_table과 bounty_table_keys 제거
+    const { bounty_table, bounty_table_keys, ...otherFields } = foundationResult.content.fields;
+    const filteredFoundationDetails = {
+        ...foundationResult,
+        content: {
+            ...foundationResult.content,
+            fields: otherFields
+        }
+    };
+
+    // bounty_table_keys의 각 키를 사용하여 bountyDetails 가져오기
+    const bountyDetailsPromises = (bounty_table_keys || []).map(async (bountyKey: string) => {
+        return await getObject(bountyKey);
+    });
+
+    const bountyDetails = await Promise.all(bountyDetailsPromises);
+
+    // bountyDetails를 foundationDetails 내부에 포함
+    const finalResult = {
+        foundationDetails: {
+            ...filteredFoundationDetails,
+            content: {
+                ...filteredFoundationDetails.content,
+                fields: {
+                    ...filteredFoundationDetails.content.fields,
+                    bountyDetails: bountyDetails.filter(Boolean) // null 값을 제외하고 반환
+                }
+            }
+        }
+    };
+
+    // digest, version, dataType, hasPublicTransfer 필드를 제거
+    const fieldsToRemove = ['digest', 'version', 'dataType', 'hasPublicTransfer'];
+    const filteredResult = filterFields(finalResult, fieldsToRemove);
+
+    res.json(filteredResult);
+}));
+
+// digest, version, dataType, hasPublicTransfer 있는 버전
+// // foundationId로 foundation과 bounty 상세 정보를 얻는 새로운 엔드포인트
+// app.get('/get-foundation-bounty-details/:foundationId', asyncHandler(async (req: Request, res: Response) => {
+//     const { foundationId } = req.params;
+
+//     if (!foundationId) {
+//         return res.status(400).send("Foundation ID is required");
+//     }
+
+//     // Foundation 객체에서 데이터 가져오기
+//     const foundationResult = await getObject(foundationId);
+
+//     if (!foundationResult) {
+//         return res.status(404).send("Foundation not found");
+//     }
+
+//     // foundationDetails에서 bounty_table과 bounty_table_keys를 제거
+//     const { bounty_table, bounty_table_keys, ...otherFields } = foundationResult.content.fields;
+//     const filteredFoundationDetails = {
+//         ...foundationResult,
+//         content: {
+//             ...foundationResult.content,
+//             fields: otherFields
+//         }
+//     };
+
+//     // bounty_table_keys의 각 키를 사용하여 bountyDetails 가져오기
+//     const bountyDetailsPromises = (bounty_table_keys || []).map(async (bountyKey: string) => {
+//         return await getObject(bountyKey);
+//     });
+
+//     const bountyDetails = await Promise.all(bountyDetailsPromises);
+
+//     // bountyDetails를 foundationDetails 내부에 포함
+//     const finalResult = {
+//         foundationDetails: {
+//             ...filteredFoundationDetails,
+//             content: {
+//                 ...filteredFoundationDetails.content,
+//                 fields: {
+//                     ...filteredFoundationDetails.content.fields,
+//                     bountyDetails: bountyDetails.filter(Boolean) // null 값을 제외하고 반환
+//                 }
+//             }
+//         }
+//     };
+
+//     res.json(finalResult);
+// }));
+
+// New endpoint to retrieve completed bounty details
+app.get('/get-completed-bounty-details/:objectId', asyncHandler(async (req: Request, res: Response) => {
+    const { objectId } = req.params;
+
+    const result = await getObject(objectId);
+    const completedIds = result.content?.fields?.completed || [];
+
+    const completedDetails = await Promise.all(
+        completedIds.map(async (completedId: string) => {
+            const data = await getObject(completedId);
+            return data;
+        })
+    );
+
+    res.json({ completedDetails });
+}));
+
+// New endpoint to retrieve processing bounty details
+app.get('/get-processing-bounty-details/:objectId', asyncHandler(async (req: Request, res: Response) => {
+    const { objectId } = req.params;
+
+    const result = await getObject(objectId);
+    const processingIds = result.content?.fields?.processing || [];
+
+    const processingDetails = await Promise.all(
+        processingIds.map(async (processingId: string) => {
+            const data = await getObject(processingId);
+            return data;
+        })
+    );
+
+    res.json({ processingDetails });
+}));
+
+// New endpoint to retrieve unconfirmed proposal bounty details
+app.get('/get-unconfirmed-bounty-details/:objectId', asyncHandler(async (req: Request, res: Response) => {
+    const { objectId } = req.params;
+
+    const result = await getObject(objectId);
+    const unconfirmedProposalIds = result.content?.fields?.unconfirmedProposal || [];
+
+    const unconfirmedProposalDetails = await Promise.all(
+        unconfirmedProposalIds.map(async (proposalId: string) => {
+            const data = await getObject(proposalId);
+            return data;
+        })
+    );
+
+    res.json({ unconfirmedProposalDetails });
 }));
 
 app.listen(port, () => {
