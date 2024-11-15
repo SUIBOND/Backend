69fc47e45541118d0d20a6c35b611ac0e2ba0d85
guboc11 <guboc11@gmail.com>
2024-11-16 02:59:22 +0900

    Update package id

--- src/config.ts
+++ src/config.ts
@@ -10,8 +10,8 @@ interface Config {
 
 // 설정 정보를 가져오는 함수
 const config: Config = {
-  package_id: "0x42487c5b6ca8ee7c1b98c6a60e1a6315f08bba6b0cd38276147ef85b69b83048",
-  platform_obj_id: "0x308a516f13f7855bac7f991ad8a023565ed3098c43211a38a8c8d06fd3b5026b",
+  package_id: "0x5be6654cea5ff00eb402a28c6fd86e95208435e50c9689301d4da1be08e87f5e",
+  platform_obj_id: "0xa3b60233ae8bf0a350418b89dff7cc312257681d22ab4be2dd8613cdacf2c831",
   testnet_endpoint: "https://rpc-testnet.suiscan.xyz:443",
 };
 
