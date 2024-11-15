b1b8dd9d0b3a8ec404019eef071735e73a429018
guboc11 <guboc11@gmail.com>
2024-11-16 08:14:48 +0900

    Update package id

--- src/config.ts
+++ src/config.ts
@@ -12,8 +12,8 @@ interface Config {
 
 // 설정 정보를 가져오는 함수
 const config: Config = {
-  package_id: '0x00ddf376f278328c1c40014223e3a07953f6d48aef026cb14ca5e7a24b2eb914',
-  platform_obj_id: '0x7b217f5be67d22704cf0278a309233aa0edd0cce5fbbf4c717cd64967d7da00f',
+  package_id: '0x922d74ee216861856fc88acf96c039658777056cad3f712e79346014a26bfaea',
+  platform_obj_id: '0xa39d4371517fee9b4677e16abb7d0369de4f75f92679e710e60f9f679125149a',
   testnet_endpoint: process.env.TESTNET_ENDPOINT || 'https://rpc-testnet.suiscan.xyz:443',
   publisher_url: process.env.PUBLISHER_URL || 'https://publisher.walrus-testnet.walrus.space',
   aggregator_url: process.env.AGGREGATOR_URL || 'https://aggregator.walrus-testnet.walrus.space',
